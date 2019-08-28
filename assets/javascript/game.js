246e0d2a7e7bab2496518e1aeab3c1db9db1882b
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2019-08-27 20:11:53 -0400

    Random number generating, game is initiating upon crystal click, stats are printing to html and changed some id's in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,76 @@
-alert("Testing Bitch");
+// alert("Testing Bitch");
+
+
+
+var wins = 0;
+var losses = 0;
+var score = 0;
+// var numberToMatch = [];
+
+// variables to get element from html in order to log stats 
+var winsText = $('#wins');
+    // console.log("wins text "+ winsText);
+var lossesText = $('#losses');
+    // console.log("losses text " + lossesText);
+var scoreText = $('#score');
+    // console.log(winsText);
+var numberToMatchText = $('#number-to-match');
+
+
+// variable to generate random number between 18 and 120
+var cpuChoice = [Math.floor(Math.random() * 102) + 19];
+    console.log(cpuChoice);
+
+// variable to generate random number between 1 and 12 (for crystals)
+var crystalNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+    console.log(crystalNumber);
+
+// function crystalNumber () {
+//     [Math.floor(Math.random() * 10) + 2];
+// }
+
+function crystalClick() {
+    
+}
+
+$(document).ready(function(){
+
+        // initiates game on crystal click 
+    $("img").click(function(){
+    
+        
+      
+
+
+
+
+    $(winsText).html("Wins: " + wins)
+        // console.log(winsText);
+    $(lossesText).html("Losses: " + losses);
+        // console.log(lossesText);
+    $(scoreText).html("Score: " + score);
+        // console.log(scoreText);
+    $(numberToMatchText).html(cpuChoice);
+        // console.log(numberToMatchText); not working in console but I get correct output
+    });
+});
+
+
+                         // Notes
+
+// Function variable to generate random Number between 19 and 120 
+// *****Should I use this or a global variable?***** 
+// var cpuChoice = function() {
+//     cpuChoice = [Math.floor(Math.random() * 102) + 19];
+// }
+// console.log(cpuChoice);  
+
+
+//  Alternative to generate random number between 18 and 120    
+// var max = 120;
+// var min = 19;
+// var cpuChoice = [Math.floor(Math.random() * (max - min + 1)) + min];
+// console.log(cpuChoice);
+
+                        // To Do
+// check video to see if CPU number to reach is there when game starts or on crytal click 
